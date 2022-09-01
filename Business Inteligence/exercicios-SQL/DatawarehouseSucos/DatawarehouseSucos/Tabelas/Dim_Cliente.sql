ddbaffe3e235c985e6da7c0df42d0c53d127e676
GZILLIG <gabrielzillig@gmail.com>
2022-08-31 22:18:34 -0300

    Fazendo cargas nas tabelas do Datawarehouse Sucos

---
+++ Business Inteligence/exercicios-SQL/DatawarehouseSucos/DatawarehouseSucos/Tabelas/Dim_Cliente.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Dim_Cliente]
+(
+	[Cod_Cliente] NVARCHAR(50) NOT NULL PRIMARY KEY,
+	[Desc_Cliente] NVARCHAR(200),
+	[Cod_Cidade] NVARCHAR(50),
+	[Desc_Cidade] NVARCHAR(200),
+	[Cod_Estado] NVARCHAR(50),
+	[Desc_Estado] NVARCHAR(200),
+	[Cod_Regiao] NVARCHAR(50),
+	[Desc_Regiao] NVARCHAR(200),
+	[Cod_Segmento] NVARCHAR(50),
+	[Desc_Segmento] NVARCHAR(200)
+)
